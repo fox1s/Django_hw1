4f895792056d3eb4d66640139123e6ca0d13db3e
fox1s <ostinsmikh@gmail.com>
2021-03-03 20:49:53 +0200

    calculator

---
+++ calc/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+import os
+import json
+
+# Create your views here.
+db_path = os.path.join('calc', 'db.json')
+
+
+# def calc(request, number1, oper, number2):
+#     res = 0
+#     if oper == '*':
+#         res = number1 * number2
+#     elif oper == '+':
+#         res = number1 + number2
+#     elif oper == '-':
+#         res = number1 - number2
+#     elif oper == 'div':
+#         res = number1 / number2
+#
+#     with open(db_path, 'w') as file:
+#         json.dump()
+#     print(res, 'result', number1, number2, oper)
+#     print(db_path)
+#     return render(request, 'calc/calc.html')
+
+def calc(request, number1, oper, number2):
+    res = 0
+    if oper == '*':
+        res = number1 * number2
+    elif oper == '+':
+        res = number1 + number2
+    elif oper == '-':
+        res = number1 - number2
+    elif oper == 'div':
+        res = number1 / number2
+
+    with open(db_path, 'w') as file:
+        json.dump({'n1': number1, 'oper': oper, 'n2': number2, 'res': res}, file)
+    with open(db_path, 'r') as file:
+        result = json.load(file)
+
+    return render(request, 'calc/calc.html', {'result': result})
