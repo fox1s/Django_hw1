bb5cb455207e06d5a9f28df640a6f2f2c0209d2e
fox1s <ostinsmikh@gmail.com>
2021-03-03 20:51:07 +0200

    calculator

--- calc/views.py
+++ calc/views.py
@@ -7,23 +7,6 @@ import json
 db_path = os.path.join('calc', 'db.json')
 
 
-# def calc(request, number1, oper, number2):
-#     res = 0
-#     if oper == '*':
-#         res = number1 * number2
-#     elif oper == '+':
-#         res = number1 + number2
-#     elif oper == '-':
-#         res = number1 - number2
-#     elif oper == 'div':
-#         res = number1 / number2
-#
-#     with open(db_path, 'w') as file:
-#         json.dump()
-#     print(res, 'result', number1, number2, oper)
-#     print(db_path)
-#     return render(request, 'calc/calc.html')
-
 def calc(request, number1, oper, number2):
     res = 0
     if oper == '*':
